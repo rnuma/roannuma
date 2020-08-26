a511381d70fa04668163714b8b151f84c32b1297
Roan Numa <56887085+rnuma@users.noreply.github.com>
2020-08-26 00:30:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.header0').height($(window).height());
+  })
+
+
+  $(document).ready(function(){
+    $('.header1').height($(window).height());
+  })
+
+
+  $(document).ready(function(){
+    $('.header2').height($(window).height());
+  })
+
+  
+  $(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+  });
